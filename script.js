a21620c2a77fed854cec85a196d4005fe46ca682
Vladyslav <melnyk12@wp.pl>
2021-12-21 23:57:49 +0100

    first_commit

---
+++ script.js
@@ -0,0 +1,53 @@
+"use strict";
+
+//alert("Hello");
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = +prompt("Do you have 18 years?", "");
+//console.log(typeof(answer));
+//console.log(answer + 5);
+//const answers = [];
+
+//answers[0] = prompt('What you nawe?', '');
+//answers[1] = prompt('What you surname?', '');
+//answers[2] = prompt('How old are you?', '');
+
+//console.log(answers);
+//document.write(answers);
+//console.log(typeof(answers));
+//console.log(typeof(null));
+///////////////////////////////////////////////////////
+
+//const category = 'toys';
+
+//console.log(`https://someurl.com/${category})/5`);
+
+//const user = 'Vlad';
+
+//alert(`Hello dear, ${user}`);
+
+console.log(4 + +'5');
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+console.log(5%2);
+
+console.log(2*4 === 8);
+
+const isChecked = true,
+        isClose = false;
+//console.log(isChecked && isClose);
+console.log(isChecked || !isClose);
+
+console.log(2 + 2 * 2 !== '6');
+
+
