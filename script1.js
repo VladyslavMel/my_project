44eff902f5c4c98e9c81e6580d2f2bfc0cc0cc59
Vladyslav <melnyk12@wp.pl>
2022-01-27 21:32:12 +0100

    task_with_cycl

--- script1.js
+++ script1.js
@@ -1,4 +1,6 @@
- const number0Films = +prompt ('How many films have you watched?', '');
+'use strict';
+
+const number0Films = +prompt ('How many films have you watched?', '');
  
 const personalMovieDB = {
     count: number0Films,
@@ -8,20 +10,88 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Last watched movie?', ''),
+/* const a = prompt('Last watched movie?', ''),
       b = prompt('How much do you rate it?', '');
      
-personalMovieDB.movies[a] = b;
+personalMovieDB.movies[a] = b; */
 
-const c = prompt('How your favorite actor?', ''),
+/* const c = prompt('How your favorite actor?', ''),
       d = prompt('How old is he?', '');
 personalMovieDB.actors[c] = d;
 
 const e = prompt('Do u know genres your film?', ''),
       f = prompt('Answer: ','');
-personalMovieDB.genres[e] = f;
+personalMovieDB.genres[e] = f; */
 
-console.log(personalMovieDB);
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Last watched movie?', ''),
+          b = prompt('How much do you rate it?', '');
+    
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done');
+    } else {
+        console.log('error');
+        i--;
+    }
+
+    
+} 
+
+  if (personalMovieDB.count < 10) {
+    console.log('Few films watched');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log('You like films');
+} else if (personalMovieDB.count >= 30) {
+    console.log('You are crazy');
+} else {
+    console.log('Error');
+}
+
+
+console.log(personalMovieDB); 
+
+/* switch (personalMovieDB.count) {
+    case 10:
+        console.log('Small');
+        break;
+    case 25:
+        console.log('Good');
+        break;
+    case 100:
+        console.log('Exellent');
+        break;
+    default 1:
+        console.log('Bad answer');
+        break;
+} */
+
+/* while (personalMovieDB.count < 10) {
+    console.log('Few...');
+    personalMovieDB.count ++;
+}
+while (personalMovieDB.count < 30) {
+    console.log('Ok, classic user');
+    personalMovieDB.count ++;
+}
+while (personalMovieDB.count < 100) {
+    console.log('Very nice');
+    personalMovieDB.count ++;
+} */
+
+/* do {
+    console.log('Small...');
+    personalMovieDB.count ++;
+} while (personalMovieDB.count < 10);
+
+do {
+    console.log('So close :)');
+    personalMovieDB.count --;
+} while (personalMovieDB.count > 15); */
+
+
+
+
 
 
 
