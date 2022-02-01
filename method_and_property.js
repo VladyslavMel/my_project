5168d07b69aff61ce97fd2a6495671c57b967936
Vladyslav <melnyk12@wp.pl>
2022-02-01 23:18:42 +0100

    lesson15

---
+++ method_and_property.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const str = 'teSt';
+/* const arr = [1, 2, 4];
+
+console.log(str[2]);
+console.log(arr.length); */
+
+/* console.log(str.toUpperCase()); */
+console.log(str.toLowerCase());
+console.log(str);
+
+const fruit = 'Same fruit';
+
+console.log(fruit.indexOf('fruit'));
+/* console.log(fruit.indexOf('Q')); */
+
+const logg = 'Vladyslav Melnyk';
+
+/* console.log(logg.slice(0,9)); */
+/* console.log(logg.slice(10));
+console.log(logg.slice(-6, -1)); */
+
+console.log(logg.substring(0, 9));
+
+console.log(logg.substr(0, 10));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const tex = '12.2px';
+console.log(parseInt(tex));
+console.log(parseFloat(tex));
+
+
+
+
+
+
+
+
+
+
